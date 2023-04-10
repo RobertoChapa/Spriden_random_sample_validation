3b5ec9adf8d314b199f26b409727d289bca7e0e5
Robert Chapa <bobby.chapa@gmail.com>
2023-04-10 10:50:16 -0400

    All items have been fixed. Code is complete.

--- Calculate_Sample_Size.cs
+++ Calculate_Sample_Size.cs
@@ -1,34 +1,46 @@
 ﻿using System;
 
+/*
+ * Bobby Chapa
+ * 4/10/2023
+ * Calculates sample size based on the formula for finite populations
+ */
+
 namespace Spriden_random_sample_validation
 {
     class SampleSizeCalculator
     {
-        private int _confidenceLevel;
+        public int _confidenceLevel { get; set; }
+        public int _populationSize { get; set; }
 
-        public SampleSizeCalculator(int confidenceLevel)
-        {
-            _confidenceLevel = confidenceLevel;
-        }
-
-        public int CalculateSampleSize(int populationSize)
-        {
+        public int CalculateSampleSize()
+        { 
             double zScore = 0.0;
 
             if (_confidenceLevel == 95)
                 zScore = 1.96;
             else if (_confidenceLevel == 99)
                 zScore = 2.58;
+            else if (_confidenceLevel == 98)
+                zScore = 2.576;
+            else if (_confidenceLevel == 90)
+                zScore = 1.465;
+            else if (_confidenceLevel == 85)
+                zScore = 1.44;
+            else if (_confidenceLevel == 80)
+                zScore = 1.282;
+            else if (_confidenceLevel == 0) // if 0 set to 95
+                zScore = 1.96;
             else
             {
-                Console.WriteLine("Invalid confidence level. Use 95 or 99.");
+                Console.WriteLine("Invalid confidence level. Use 80,85,90,95,98 or 99.");
                 return -1;
             }
 
             double estimatedProportion = 0.5;
             double marginOfError = 0.05;
 
-            int sampleSize = ComputeSampleSize(populationSize, zScore, estimatedProportion, marginOfError);
+            int sampleSize = ComputeSampleSize(_populationSize, zScore, estimatedProportion, marginOfError);
             return sampleSize;
         }
 
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Spriden_random_sample_validation;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -17,34 +18,67 @@ namespace SpridenRandomSampleValidation
     {
         static void Main(string[] args)
         {
-            string fileName = "SPRIDENV_edited";
-            int setSize = 278; // sample size
-            string filePath = @"Y:\Director\CSRS_Banner_Validation\" + fileName + ".dat";
-
-            //Console.WriteLine("Enter the population size:");
-            int populationSize = 200000;//int.Parse(Console.ReadLine());
-
-            //Console.WriteLine("Enter the confidence level (95 or 99):");
-            int confidenceLevel = 95;//int.Parse(Console.ReadLine());
+            string fileName = "";//"SPRIDEN_edited";
+            int setSize = 0; // sample size
+            int confidenceLevel = 0;
+            string filePath = "";
 
             try
             {
+                Console.WriteLine("Enter file name minus extension:");
+                fileName = Console.ReadLine();
+                filePath = @"Y:\Director\CSRS_Banner_Validation\" + fileName + ".dat";
+
+                Console.WriteLine("Enter the Confidence Level 80, 85, 90, 95, 98, 99\r");
+                if (int.TryParse(Console.ReadLine(), out confidenceLevel) && (confidenceLevel == 80 || confidenceLevel == 85 || confidenceLevel == 90 || confidenceLevel == 95 || confidenceLevel == 98 || confidenceLevel == 99))
+                {
+                    Console.WriteLine("\nYou entered: " + confidenceLevel);
+                }
+                else
+                {
+                    Console.WriteLine("Default value of 95 will be used.");
+                    confidenceLevel = 95;
+                }
+
+                // read dat file and retrieve first field per line
                 SpridenFileReader fileReader = new SpridenFileReader(filePath);
                 List<string> spridenIdList = fileReader.ReadSpridenIds();
 
+                // find population size
+                int populationSize = spridenIdList.Count-1; // do not count first row. it is the column names
+
+                // set population size and confidence level
+                var sampleSizeCalculator = new Spriden_random_sample_validation.SampleSizeCalculator
+                {
+                    _confidenceLevel = confidenceLevel,
+                    _populationSize = populationSize
+                };
+
+                // calculate sample size amd output user information to console
+                Console.WriteLine("\nPopulation size: ");
+                Console.WriteLine(populationSize);
+                Console.WriteLine("\nSample size: ");
+                setSize = sampleSizeCalculator.CalculateSampleSize(); // calc sample size
+                Console.WriteLine(setSize);
+                Console.ReadLine();
+
+                // calculate the sample from the dat file
                 RandomSampleGenerator generator = new RandomSampleGenerator(spridenIdList, setSize);
                 List<string> randomSample = generator.GetRandomSample();
 
+                // write sample output to text files
                 SampleFileWriter sampleFileWriter = new SampleFileWriter(Path.GetDirectoryName(filePath), fileName);
                 sampleFileWriter.WriteSampleToFile(randomSample);
             }
             catch (FileNotFoundException)
             {
                 Console.WriteLine($"The file '{filePath}' was not found.");
+                Console.ReadLine();
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"An error occurred: {ex.Message}");
+                Console.ReadLine();
             }
         }
     }
@@ -125,7 +159,7 @@ namespace SpridenRandomSampleValidation
         }
     }
 
-    // writes the random sample to a text file
+    // write the random sample to a text file
     public class SampleFileWriter
     {
         private readonly string _fileDirectory;
